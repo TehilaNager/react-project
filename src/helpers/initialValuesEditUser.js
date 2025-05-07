2a60bb01d3c768b88ce1924e635ee50c4ca9ec31
TehilaNager <tehila6619366@gmail.com>
2025-05-08 00:01:53 +0300

    hhh

--- src/helpers/initialValuesEditUser.js
+++ src/helpers/initialValuesEditUser.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
 import { useAuth } from "../context/authContext";
 
 function initialValuesEdituser() {
     const { user, userState, initialValueEdit } = useAuth();
+
+    console.log(initialValueEdit.name.first);
+
 
     if (user.isAdmin === true) {
         return ({
@@ -22,21 +24,21 @@ function initialValuesEdituser() {
         })
     }
 
-    return ({
-        first: initialValueEdit.name.first,
-        middle: initialValueEdit.name.middle,
-        last: initialValueEdit.name.last,
-        phone: initialValueEdit.phone,
-        url: initialValueEdit.image.url,
-        alt: initialValueEdit.image.alt,
-        state: initialValueEdit.address.state,
-        country: initialValueEdit.address.country,
-        city: initialValueEdit.address.city,
-        street: initialValueEdit.address.street,
-        houseNumber: initialValueEdit.address.houseNumber,
-        zip: initialValueEdit.address.zip,
-        isBusiness: initialValueEdit.isBusiness
-    })
+    // return ({
+    //     first: initialValueEdit.name.first,
+    //     middle: initialValueEdit.name.middle,
+    //     last: initialValueEdit.name.last,
+    //     phone: initialValueEdit.phone,
+    //     url: initialValueEdit.image.url,
+    //     alt: initialValueEdit.image.alt,
+    //     state: initialValueEdit.address.state,
+    //     country: initialValueEdit.address.country,
+    //     city: initialValueEdit.address.city,
+    //     street: initialValueEdit.address.street,
+    //     houseNumber: initialValueEdit.address.houseNumber,
+    //     zip: initialValueEdit.address.zip,
+    //     isBusiness: initialValueEdit.isBusiness
+    // })
 }
 
 export default initialValuesEdituser;
