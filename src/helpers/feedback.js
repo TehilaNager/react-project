92d5e25f5392e22b68ff5b480926ff443e580cd5
TehilaNager <tehila6619366@gmail.com>
2025-05-15 14:40:56 +0300

    j

--- src/helpers/feedback.js
+++ src/helpers/feedback.js
@@ -29,38 +29,4 @@ export const errorFeedback = (message) => {
     });
 }
 
-export const deleteFeedback = (itemName = "item") => {
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-            confirmButton: "btn btn-success",
-            cancelButton: "btn btn-danger"
-        },
-        buttonsStyling: false
-    });
-
-    swalWithBootstrapButtons.fire({
-        title: `Delete this ${itemName}?`,
-        text: `This ${itemName} will be permanently removed. Are you sure?`,
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonText: "Yes, delete it!",
-        cancelButtonText: "No, cancel!",
-        reverseButtons: true
-    }).then((result) => {
-        if (result.isConfirmed) {
-            swalWithBootstrapButtons.fire({
-                title: "Deleted!",
-                text: `The ${itemName} has been successfully deleted.`,
-                icon: "success"
-            });
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-            swalWithBootstrapButtons.fire({
-                title: "Cancelled",
-                text: `The ${itemName} was not deleted.`,
-                icon: "error"
-            });
-        }
-    });
-};
-
 export default feedback;
